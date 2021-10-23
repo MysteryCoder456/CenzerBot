edb8013a1d0ef982aa542291db568de8245c21e6
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-10-23 09:59:33 +0400

    unused import

--- bot/db/models.py
+++ bot/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Boolean, JSON, Text, CHAR, Enum
+from sqlalchemy import Column, Integer, Boolean, JSON, CHAR, Enum
 from sqlalchemy.ext.declarative import declarative_base
 
 from bot.enums import CensorMode
